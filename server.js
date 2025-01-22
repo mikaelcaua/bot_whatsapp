f1bf8879e6160978106390266639c531e2cbb24b
mikaelcaua <mikaelcauasilva1@gmail.com>
2025-01-22 09:31:43 -0300

    feat: use routes in server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-// server.js
 import express from 'express';
 import bodyParser from 'body-parser';
-import { sendMessage } from './controller/messageController.js';
+import messageRoutes from './routes/messageRoutes.js';
+import webhookRoutes from './routes/webhookRoutes.js';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-app.post('/send-message', sendMessage);
+app.use('/message', messageRoutes);
+app.use('/webhook', webhookRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
